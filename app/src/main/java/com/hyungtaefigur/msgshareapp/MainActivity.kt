5239220bdf724ca1b60ffd99a77587806657f28e
Hyung Tae C. Figur <hyu_03@hotmail.com>
2020-02-14 17:34:38 -0300

    -

--- app/src/main/java/com/hyungtaefigur/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/hyungtaefigur/msgshareapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.hyungtaefigur.msgshareapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Message
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -14,19 +15,29 @@ class MainActivity : AppCompatActivity() {
 
         btnShowToast.setOnClickListener(){
             Log.i("MainActivity","Button was clicked!")
-            txvTest.text = etUserMessage.text.toString()
             Toast.makeText(this,"Button was clicked!",Toast.LENGTH_SHORT).show()
         }
 
         btnSendMsgNextActivity.setOnClickListener(){
             val message: String = etUserMessage.text.toString()
 
-            //val intent = Intent(this, SecondActivity::class.java)
-            val intent = Intent(this, AnotherActivity::class.java)
+            val intent = Intent(this, SecondActivity::class.java)
+            //val intent = Intent(this, AnotherActivity::class.java)
 
             intent.putExtra("user_message", message)
 
             startActivity(intent)
+        }
+
+        btnShareToOtherApps.setOnClickListener{
+            val message: String = etUserMessage.text.toString()
+
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            intent.type = "text/plain"
+
+            startActivity(Intent.createChooser(intent,"Please select app: "))
         }
 
     }
